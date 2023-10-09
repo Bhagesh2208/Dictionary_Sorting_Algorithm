99348afec01021a4e63e81a2f3005c29790d217f
Bhagesh <70843231+Bhagesh2208@users.noreply.github.com>
2023-10-09 19:37:55 +0530

    Update Main.java
    small update

--- Main.java
+++ Main.java
@@ -7,7 +7,10 @@ public class Main {
 
         String [] smr = new String[]{"1","2","3"};
         Dictionary_Sorting_Algorithm f = new Dictionary_Sorting_Algorithm();
-        f.StartSorting("3 1 1 4 21 26 78 15 98 78 666 100000 522");
+        ArrayList<String> sorted_ArrayList = f.StartSorting("3 1 1 4 21 26 78 15 98 78 666 100000 522");
+
+        for(String s: sorted_ArrayList)
+            System.out.println(s);
     }
 
 
